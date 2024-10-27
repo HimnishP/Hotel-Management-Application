0d32255340100d7f905dab0a00f34d84a619dab5
HimnishP <2360165@edu.vaniercollege.qc.ca>
2024-10-27 15:31:58 -0400

    -Implemented HotelManagementSystem (singleton)

--- src/main/java/com/hotelmanagementapplication/model/HotelManagementSystem.java
+++ src/main/java/com/hotelmanagementapplication/model/HotelManagementSystem.java
@@ -2,5 +2,20 @@ package com.hotelmanagementapplication.model;
 
 public class HotelManagementSystem {
     private static HotelManagementSystem instance;
+    //TODO declare all of the lists , maps or sets here
 
+    private HotelManagementSystem() {
+        //TODO initialize them here
+    }
+
+    public static HotelManagementSystem getInstance() {
+        if (instance == null) {
+            synchronized (HotelManagementSystem.class) {
+                if (instance == null) {
+                    instance = new HotelManagementSystem();
+                }
+            }
+        }
+        return instance;
+    }
 }
