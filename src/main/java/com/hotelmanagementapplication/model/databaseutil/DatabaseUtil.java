9fa8d6bc17f6586f91441c29d7dfbebf11cd51bb
Parth Patel <parthschool20s@gmail.com>
2024-11-13 11:14:49 -0500

    Made DatabaseUtil and organized the packages

---
+++ src/main/java/com/hotelmanagementapplication/model/databaseutil/DatabaseUtil.java
@@ -0,0 +1,9 @@
+package com.hotelmanagementapplication.model.databaseutil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil {
+
+}
