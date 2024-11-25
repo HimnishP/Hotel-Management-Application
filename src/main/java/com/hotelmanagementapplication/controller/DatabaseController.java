7b202f6c2becfdcacf2e1de47397a8b2d2e2bd44
HimnishP <2360165@edu.vaniercollege.qc.ca>
2024-11-25 18:14:17 -0500

    Made the database logic more abstract (created PaymentDatabase and UserDatabase to centralize the logic) , and they are being called in the main DB controller

--- src/main/java/com/hotelmanagementapplication/controller/DatabaseController.java
+++ src/main/java/com/hotelmanagementapplication/controller/DatabaseController.java
@@ -1,6 +1,10 @@
 package com.hotelmanagementapplication.controller;
 
+import com.hotelmanagementapplication.controller.utildatabase.PaymentDatabase;
 import com.hotelmanagementapplication.controller.utildatabase.UserDatabase;
+import com.hotelmanagementapplication.model.payment.CreditCardPayment;
+import com.hotelmanagementapplication.model.payment.DebitCardPayment;
+import com.hotelmanagementapplication.model.payment.Payment;
 import com.hotelmanagementapplication.model.user.Customer;
 import com.hotelmanagementapplication.model.user.Manager;
 import com.hotelmanagementapplication.model.user.User;
@@ -120,5 +124,112 @@ public class DatabaseController {
      */
     public static void alterUser(int id, User user) {
         UserDatabase.alterUser(id, user);
+    }
+
+    /**
+     * Creates the Payment table in the database.
+     * This table holds basic payment information for each payment.
+     */
+    public void createTablePayment() {
+        PaymentDatabase.createTablePayment();
+    }
+
+    /**
+     * Creates the DebitCardPayment table in the database.
+     * This table holds specific details for debit card payments.
+     */
+    public void createTableDebitCardPayment() {
+        PaymentDatabase.createTableDebitCardPayment();
+    }
+
+    /**
+     * Creates the CreditCardPayment table in the database.
+     * This table holds specific details for credit card payments.
+     */
+    public void createTableCreditCardPayment() {
+        PaymentDatabase.createTableCreditCardPayment();
+    }
+
+    /**
+     * Inserts a payment into the Payment table.
+     *
+     * @param payment     The payment object containing payment details.
+     * @param paymentType The type of the payment (e.g., "DebitCardPayment" or "CreditCardPayment").
+     * @return The generated payment ID.
+     */
+    public int insertPayment(Payment payment, String paymentType) {
+        return PaymentDatabase.insertPayment(payment, paymentType);
+    }
+
+    /**
+     * Inserts a debit card payment into the DebitCardPayment table.
+     * This method takes a debit card payment object and inserts the details into the database.
+     *
+     * @param debit The DebitCardPayment object containing payment details.
+     */
+    public void insertDebitCardPayment(DebitCardPayment debit) {
+        PaymentDatabase.insertDebitCardPayment(debit);
+    }
+
+    /**
+     * Inserts a credit card payment into the CreditCardPayment table.
+     * This method takes a credit card payment object and inserts the details into the database.
+     *
+     * @param credit The CreditCardPayment object containing payment details.
+     */
+    public void insertCreditCardPayment(CreditCardPayment credit) {
+        PaymentDatabase.insertCreditCardPayment(credit);
+    }
+
+    /**
+     * Retrieves a list of all payments from the Payment table.
+     * This method retrieves all payment entries, including both debit and credit card payments.
+     *
+     * @return A list of Payment objects containing basic payment details.
+     */
+    public List<Payment> selectPayments() {
+        return PaymentDatabase.selectPayments();
+    }
+
+    /**
+     * Retrieves a list of all debit card payments.
+     * This method retrieves all debit card payment records from the database.
+     *
+     * @return A list of DebitCardPayment objects containing debit card payment details.
+     */
+    public List<DebitCardPayment> selectDebitCardPayments() {
+        return PaymentDatabase.selectDebitCardPayments();
+    }
+
+    /**
+     * Retrieves a list of all credit card payments.
+     * This method retrieves all credit card payment records from the database.
+     *
+     * @return A list of CreditCardPayment objects containing credit card payment details.
+     */
+    public List<CreditCardPayment> selectCreditCardPayments() {
+        return PaymentDatabase.selectCreditCardPayments();
+    }
+
+    /**
+     * Retrieves a specific debit card payment based on the payment ID.
+     * This method fetches details of a single debit card payment by its unique payment ID.
+     *
+     * @param paymentId The ID of the debit card payment to retrieve.
+     * @return The DebitCardPayment object corresponding to the payment ID.
+     */
+    public DebitCardPayment selectDebitCardPayment(int paymentId) {
+        return PaymentDatabase.selectDebitCardPayment(paymentId);
+    }
+
+    /**
+     * Retrieves a specific credit card payment based on the payment ID.
+     * This method fetches details of a single credit card payment by its unique payment ID.
+     *
+     * @param paymentId The ID of the credit card payment to retrieve.
+     * @return The CreditCardPayment object corresponding to the payment ID.
+     */
+    public CreditCardPayment selectCreditCardPayment(int paymentId) {
+        return PaymentDatabase.selectCreditCardPayment(paymentId);
     }
 }
