fe2624d267b93ef11993c045401bd01b88e25907
HimnishP <2360165@edu.vaniercollege.qc.ca>
2024-11-25 19:26:19 -0500

    Create method to addPayment

--- src/main/java/com/hotelmanagementapplication/model/system/PaymentSystem.java
+++ src/main/java/com/hotelmanagementapplication/model/system/PaymentSystem.java
@@ -8,13 +8,14 @@ import com.hotelmanagementapplication.model.payment.Payment;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 public class PaymentSystem {
     private final Map<Integer, Payment> paymentMap;
     private final Map<Integer, CreditCardPayment> cardPaymentMap;
     private final Map<Integer, DebitCardPayment> debitCardPaymentMap;
-    private final ExecutorService executorService = Executors.newCachedThreadPool();
+    private final ExecutorService EXECUTOR_SERVICE = Executors.newCachedThreadPool();
 
     public PaymentSystem() {
         paymentMap = DatabaseController.selectPayments()
@@ -26,5 +27,27 @@ public class PaymentSystem {
         debitCardPaymentMap = DatabaseController.selectDebitCardPayments()
                 .stream()
                 .collect(Collectors.toMap(DebitCardPayment::getPaymentId, payment -> payment));
+    }
+
+    /**
+     * Adds a payment to the system.
+     *
+     * @param payment the payment to add
+     * @return the Future of the operation
+     */
+    public Future<Void> addPayment(Payment payment) {
+        return EXECUTOR_SERVICE.submit(() -> {
+            if (payment instanceof CreditCardPayment) {
+                DatabaseController.insertPayment(payment, "CreditCardPayment");
+                DatabaseController.insertCreditCardPayment((CreditCardPayment) payment);
+            } else if (payment instanceof DebitCardPayment) {
+                DatabaseController.insertPayment(payment, "DebitCardPayment");
+                DatabaseController.insertDebitCardPayment((DebitCardPayment) payment);
+            } else {
+                throw new IllegalArgumentException("Invalid payment type");
+            }
+            paymentMap.put(payment.getUser().getUserId(), payment);
+            return null;
+        });
     }
 }
