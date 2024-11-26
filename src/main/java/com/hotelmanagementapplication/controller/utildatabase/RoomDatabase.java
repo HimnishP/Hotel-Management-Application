55ca904544b5f791aad066f97b19d9e9b6292955
Parth Patel <parthschool20s@gmail.com>
2024-11-26 14:01:55 -0500

    Made a createRoomTable method.

--- src/main/java/com/hotelmanagementapplication/controller/utildatabase/RoomDatabase.java
+++ src/main/java/com/hotelmanagementapplication/controller/utildatabase/RoomDatabase.java
@@ -1,8 +1,22 @@
 package com.hotelmanagementapplication.controller.utildatabase;
 
+import static com.hotelmanagementapplication.controller.utildatabase.DatabaseUtil.executeUpdate;
+
 public class RoomDatabase {
 
+    /**
+     * Creates Room table.
+     */
     public static void createRoomTable(){
-
+        String sql = """
+                CREATE TABLE IF NOT EXISTS Rooms (
+                roomId INTEGER PRIMARY KEY AUTOINCREMENT,
+                room_price REAL NOT NULL,
+                room_status TEXT NOT NULL,
+                room_type TEXT NOT NULL,
+                )
+                """;
+        executeUpdate(sql);
     }
+
 }
