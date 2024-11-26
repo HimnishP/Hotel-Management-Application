e33448e91676b91d2370d0d6012dff5997a4c129
HimnishP <2360165@edu.vaniercollege.qc.ca>
2024-11-25 21:20:12 -0500

    Made localization properties , added documentation , added methods in controller

--- src/main/java/com/hotelmanagementapplication/controller/DebitPaymentScreenController.java
+++ src/main/java/com/hotelmanagementapplication/controller/DebitPaymentScreenController.java
@@ -1,4 +1,34 @@
 package com.hotelmanagementapplication.controller;
 
+import com.hotelmanagementapplication.controller.l10n_i18n.ScreenHandler;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DebitPaymentScreenController {
+    @FXML
+    private TextField cardHolderNameTF;
+    @FXML
+    private TextField expirationDateTF;
+    @FXML
+    private TextField securityCodeTF;
+    @FXML
+    private TextField debitCardNumberTF;
+
+    public void validateButtonHandler(ActionEvent actionEvent) {
+    }
+
+    /**
+     * Method will switch back to the welcome screen
+     *
+     * @param actionEvent The event
+     */
+    public void handleReturnToWelcomeScreenButton(ActionEvent actionEvent) throws IOException {
+        Stage primaryStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        ScreenHandler.switchScreens(primaryStage, "WelcomeScreen.fxml");
+    }
 }
